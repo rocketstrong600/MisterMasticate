d8981df54dba837a23ec4c866d20749773667ab9
Ethan Johnston <rocketstrong600@gmail.com>
2019-12-03 17:11:28 +1000

    new db

--- MisterMasticate/__init__.py
+++ MisterMasticate/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import hiddendata
+import os
+
+cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Change Me'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = SECRET
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CON_URL
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
